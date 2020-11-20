2b4c6b5ea92e5408f12f7ed8aba1fb4fcfbf1d58
Annette WIedenmann <rina.chick@web.de>
2020-11-20 19:40:21 +0100

    aufgabe 6

---
+++ Aufgabe_06/Hexenkessel/Client/client.js
@@ -0,0 +1,34 @@
+var L06_Hexenkessel;
+(function (L06_Hexenkessel) {
+    async function getData() {
+        let response = await fetch("data.json");
+        let content = await response.text();
+        let data = JSON.parse(content);
+        console.log("DATA:");
+        console.log(data);
+        L06_Hexenkessel.generateContent(data);
+    }
+    L06_Hexenkessel.getData = getData;
+    async function sendPotion(_event) {
+        console.log("send Potion");
+        let url = "https://annettewdm.herokuapp.com/";
+        /* let url: string = "http://localhost:5001/"; */
+        let formData = new FormData(document.forms[0]);
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        console.log(url);
+        let select = document.querySelector("select");
+        let textarea = document.querySelector("textarea");
+        if (select)
+            url += "&Wirkung=" + select.value;
+        if (textarea.value != "")
+            url += "&Nebenwirkungen=" + textarea.value;
+        let response = await fetch(url);
+        console.log(response);
+        let responseReply = await response.text();
+        console.log(responseReply);
+        alert("Sent Potion to Professor Snape!");
+    }
+    L06_Hexenkessel.sendPotion = sendPotion;
+})(L06_Hexenkessel || (L06_Hexenkessel = {}));
+//# sourceMappingURL=client.js.map
---
+++ Aufgabe_06/Hexenkessel/Client/client.ts
@@ -0,0 +1,34 @@
+namespace L06_Hexenkessel {
+    export async function getData(): Promise<void> {
+        let response: Response = await fetch("data.json"); 
+        let content: string = await response.text();
+        let data = JSON.parse(content);  
+        console.log("DATA:");
+        console.log(data);
+
+        generateContent(data);    
+    }
+    export async function sendPotion(_event: Event): Promise<void> {
+        
+        console.log("send Potion");
+        let url: string = "https://annettewdm.herokuapp.com/";
+        /* let url: string = "http://localhost:5001/"; */
+        let formData: FormData = new FormData(document.forms[0]);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        console.log(url);
+
+        let select: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+        
+        let textarea: HTMLTextAreaElement = <HTMLTextAreaElement>document.querySelector("textarea");
+        if (select)
+            url += "&Wirkung=" + select.value;
+        if (textarea.value != "")
+            url += "&Nebenwirkungen=" + textarea.value;
+        let response: Response = await fetch(url);
+        console.log(response);
+        let responseReply: string = await response.text();
+        console.log(responseReply);
+        alert("Sent Potion to Professor Snape!");
+    }
+}
